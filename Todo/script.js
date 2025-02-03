f176c6ba24b31efb467cedae31215089f98ba561
UnejucatedGuy <UnejucatedGuy@users.noreply.github.comgit config --global user.email UnejucatedGuy@users.noreply.github.com>
2025-02-03 22:41:08 +0530

    added inplace edit functionality

--- Todo/script.js
+++ Todo/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const ulTodo = document.getElementById("ul-todo");
 
   let editMode = false;
-  let editElement = null;
+  //let editElement = null;
 
   buttonTodo.addEventListener("click", () => {
     const text = inputTodo.value;
@@ -23,11 +23,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const createTodo = (task) => {
     const li = document.createElement("li");
     li.className =
-      "list-group-item d-flex justify-content-between align-items-start";
+      "list-group-item d-flex justify-content-between align-items-around";
     li.innerHTML = `<span class="text-todo">${task}</span>
+    <input type="text" class="form-control d-none edit-todo" id="edit-todo" />
     <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-      <button type="button" class="btn btn-warning">Edit</button>
-      <button type="button" class="btn btn-danger">Delete</button>
+      <button type="button" class="btn btn-warning">edit</button>
+      <button type="button" class="btn btn-danger">delete</button>
     </div>`;
     ulTodo.appendChild(li);
   };
@@ -39,14 +40,25 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     if (e.target.classList.contains("btn-warning")) {
-      const li = e.target.closest(".list-group-item");
-      const taskText = li.querySelector(".text-todo").textContent;
-
-      inputTodo.value = taskText;
-      buttonTodo.textContent = "Update";
-
-      editElement = li;
-      editMode = true;
+      const li = e.target.closest(".list-group-item")
+      const taskTextEl = li.querySelector(".text-todo")
+      const EditTaskEl = li.querySelector(".edit-todo")
+      
+      if (e.target.textContent == "edit") {
+        const taskText = taskTextEl.textContent;
+        taskTextEl.classList.add("d-none")
+        EditTaskEl.classList.remove("d-none")
+        EditTaskEl.value = taskText
+        EditTaskEl.focus()
+        e.target.textContent = "update"  
+      } else {
+        const taskText = EditTaskEl.value
+        EditTaskEl.classList.add("d-none")
+        taskTextEl.classList.remove("d-none")
+        taskTextEl.innerHTML = taskText
+        e.target.textContent = "edit"
+        
+      }
     }
   });
 
